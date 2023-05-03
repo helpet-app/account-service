cd0e857cef4c7f2ad26f07eaa284c69106a8b30d
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-05-03 15:19:25 +0700

    Add order for sessions

--- src/main/java/com/helpet/service/account/storage/repository/SessionRepository.java
+++ src/main/java/com/helpet/service/account/storage/repository/SessionRepository.java
@@ -9,5 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface SessionRepository extends CrudRepository<Session, UUID> {
-    List<Session> findAllByAccountId(UUID accountId);
+    List<Session> findAllByAccountIdOrderByIssuedAtDesc(UUID accountId);
 }
